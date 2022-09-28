c23581a1529cd9e123cf0ae9e0ff3b7d59c67e0f
Adolfo Huerta <p6rd8bj8m6@privaterelay.appleid.com>
2022-09-28 19:18:45 +0000

    Update 5 files

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,14 +2,31 @@ import React from "react";
 import { Hello } from "./Hello.jsx";
 import { Info } from "./Info.jsx";
 import "../../public/main.css";
-export const App = () => (
-  <>
-    <div className="box">
-      <h1>Hola Mundo en MeteorJS!</h1>
-    </div>
+import { TasksCollection } from "/imports/api/TasksCollection";
+import { TaskForm } from "./TaskForm";
+import { Task } from "./Task";
 
-    <div>
-      <p>Nombre</p>
-    </div>
-  </>
-);
+export const App = () => {
+  const tasks = useTracker(() =>
+    TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+  );
+  return (
+    <>
+      <div className="box">
+        <h1>Hola Mundo en MeteorJS!</h1>
+        <Info />
+      </div>
+
+      <div>
+        <p>Nombre</p>
+        <TaskForm />
+
+        <ul>
+          {tasks.map((task) => (
+            <Task key={task._id} task={task} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
--- server/main.js
+++ server/main.js
@@ -13,23 +13,8 @@ Meteor.startup(() => {
   // If the Links collection is empty, add some data.
   if (LinksCollection.find().count() === 0) {
     insertLink({
-      title: "Do the Tutorial",
-      url: "https://www.meteor.com/tutorials/react/creating-an-app"
-    });
-
-    insertLink({
-      title: "Follow the Guide",
-      url: "http://guide.meteor.com"
-    });
-
-    insertLink({
-      title: "Read the Docs",
-      url: "https://docs.meteor.com"
-    });
-
-    insertLink({
-      title: "Discussions",
-      url: "https://forums.meteor.com"
+      title: "Haz el tutorial",
+      url: "https://www.meteor.com/tutorials/react/creating-an-app",
     });
   }
 
@@ -37,7 +22,7 @@ Meteor.startup(() => {
     crearMascota({
       nombre: "Adolfo",
       mascota: "Gato",
-      edad: 5
+      edad: 5,
     });
   }
 });
